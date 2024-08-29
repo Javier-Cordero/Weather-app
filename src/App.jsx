743854dd6c4c7d97fe32ec1246748fd4cfc58c66
Javier <javier.jc942@gmail.com>
2024-08-28 21:59:51 -0400

    weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
+import { useState, useEffect } from 'react';
 import { Header } from './components/Header';
-import { Main } from './components/Main';
+import { Container } from './components/Container';
+import axios from 'axios';
+const apiKey = 'bd9dc44134d81a9ff53c6b13a921e023';
 export default function App() {
+  const [error, setError] = useState(null)
+  const [location, setLocation] = useState('London')
+  const [units, setUnits] = useState('metric')
+  const [weather, setWeather] = useState(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=${units}&lang=en`)
+  const [forecast, setForecast] = useState(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=${units}&lang=en`);
+  const [pronostico, setPronostico] = useState({})
+  const [clima, setClima] = useState({})
+  const handleGetLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setWeather(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}&lang=en`)
+          setForecast(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}&lang=en`)
+        }, (error) => { setError(error.message) }
+      );
+    } else setError('Geolocation is not supported by this browser.')
+  };
+  const fetchWeatherData = async () => {
+    const rs = await axios.get(weather)
+    setClima({
+      id: rs.data.weather[0].id,
+      icon: rs.data.weather[0].icon,
+      temperature: Math.round(rs.data.main.temp),
+      description: rs.data.weather[0].description,
+      city: rs.data.name,
+      windSpeed: rs.data.wind.speed,
+      windDegree: rs.data.wind.deg,
+      humidity: rs.data.main.humidity,
+      visibility: rs.data.visibility,
+      air: rs.data.main.pressure
+    })
+  }
+  useEffect(() => { fetchWeatherData() }, [weather, units]);
   return (
     <div className="w-screen lg:flex">
-      <Header/>
-      <Main/>
+      <Header handleGetLocation={handleGetLocation} temperature={clima.temperature} description={clima.description} city={clima.city} />
+      <Container setUnits={setUnits} pronostico={pronostico} windSpeed={clima.windSpeed} windDegree={clima.windDegree} humidity={clima.humidity} visibility={clima.visibility} air={clima.air} />
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,31 +3,34 @@ import { Image } from 'primereact/image';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
 import { useState } from 'react';
-export const Header = () => {
+export const Header = ({ handleGetLocation,temperature,description,city }) => {
     const [visible, setVisible] = useState(false)
+    const today = new Date();
+    const options = { weekday: 'short', month: 'short', day: 'numeric' };
+    const formatDate = today.toLocaleString('en-EN', options);
     return (
         <>
             <header className='bg-[#1E213A] w-screen h-[800px] lg:w-[30%] lg:h-screen'>
                 <div className='w-full h-[14%] flex justify-between items-center px-[5%]'>
                     <Button label='Seach for places' onClick={() => setVisible(true)} className='bg-[#62636F] w-[150px] h-[40px] font-500 text-white' />
-                    <Image src="location.svg" alt="icono de localizacion" className='size-10' />
+                    <Image src="location.svg" alt="icono de localizacion" className='size-10' onClick={handleGetLocation} />
                 </div>
                 <div className='w-full h-[30%] flex items-center justify-center relative'>
                     <img className='w-full h-full opacity-5 object-cover' src='Cloud-background.png' alt="Cloud background" />
                     <img className='size-[40%] absolute' src='Shower.png' alt="Weather icon" />
                 </div>
                 <div className='w-full h-[20%] flex justify-center items-center font-500'>
-                    <span className='text-9xl'>15</span>
+                    <span className='text-8xl'>{temperature}</span>
                     <span className='text-5xl'>°C</span>
                 </div>
                 <div className='w-full h-[15%] flex justify-center items-center'>
-                    <span className='text-4xl font-600 text-[#88869D]'>Shower</span>
+                    <span className='text-4xl font-600 text-[#88869D]'>{description}</span>
                 </div>
                 <div className='w-full h-[13%] flex justify-center items-center'>
-                    <span className='font-500 text-lg text-[#88869D]'>Today • Fri, 5 Jun</span>
+                    <span className='font-500 text-lg text-[#88869D]'>{`Today • ${formatDate}`}</span>
                 </div>
                 <div className='w-full h-[8%] flex justify-center items-center text-lg'>
-                    <span className='pi pi-map-marker h-[20%]'>Helsinki</span>
+                    <span className='pi pi-map-marker h-[20%]'>{city}</span>
                 </div>
             </header>
             <Dialog visible={visible} className='bg-[#1E213A] absolute left-0 top-0 w-screen h-[600px] lg:w-[30%]' onHide={() => { if (!visible) return; setVisible(false); }}>
