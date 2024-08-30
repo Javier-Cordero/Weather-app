a94506f6ac6278904c4fc19215a56ea69620fdf3
Javier <javier.jc942@gmail.com>
2024-08-30 11:30:49 -0400

    correccion en la url de la api y mejora del diseño en la barra de busqueda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Button } from 'primereact/button';
 import { Image } from 'primereact/image';
-import { Dialog } from 'primereact/dialog';
+import { Sidebar } from 'primereact/sidebar';
 import { InputText } from 'primereact/inputtext';
 import { useState } from 'react';
 import axios from 'axios';
@@ -9,7 +9,7 @@ export const Header = ({ handleGetLocation, setLocation, simbolo, icon, temperat
     const [cityInput, setCityInput] = useState('')
     const [direct, setDirect] = useState('')
     const handleInput = async () => {
-        const rs = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=10&appid=bd9dc44134d81a9ff53c6b13a921e023`)
+        const rs = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=10&appid=bd9dc44134d81a9ff53c6b13a921e023`)
         const search = rs.data
         const filtered = search.map(result => ({
             name: result.name,
@@ -33,7 +33,7 @@ export const Header = ({ handleGetLocation, setLocation, simbolo, icon, temperat
                 </div>
                 <div className='w-full h-[30%] flex items-center justify-center relative'>
                     <img className='w-full h-full opacity-5 object-cover' src='Cloud-background.png' alt="Cloud background" />
-                    <img className='size-[40%] absolute' src={icon} alt="Weather icon" />
+                    <img className='w-28 absolute' src={icon} alt="Weather icon" />
                 </div>
                 <div className='w-full h-[20%] flex justify-center items-center font-500'>
                     <span className='text-8xl'>{temperature}</span>
@@ -49,15 +49,22 @@ export const Header = ({ handleGetLocation, setLocation, simbolo, icon, temperat
                     <span className='pi pi-map-marker h-[20%]'>{city}</span>
                 </div>
             </header>
-            <Dialog visible={visible} className='bg-[#1E213A] absolute left-0 top-0 w-screen h-[600px] lg:w-[30%]' onHide={() => { if (!visible) return; setVisible(false); }}>
+            <Sidebar visible={visible} className='bg-[#1E213A] absolute left-0 top-0 w-screen h-[600px] lg:w-[30%] lg:h-screen text-base' onHide={() => { if (!visible) return; setVisible(false); }}>
                 <div className='flex flex-wrap gap-4 justify-center'>
-                    <InputText value={cityInput} onChange={(e) => setCityInput(e.target.value)} type="search" placeholder="search location" className='h-10 w-[60%] pl-5' />
-                    <Button label="Search" onClick={handleInput} className='bg-[#3C47E9] h-10 w-[30%]' />
+                    <InputText value={cityInput} onChange={(e) => setCityInput(e.target.value)} type="search" placeholder="search location" className='h-10 w-[60%] pl-5 pr-1 bg-transparent border rounded-none' />
+                    <Button label="Search" onClick={handleInput} className='bg-[#3C47E9] h-10 w-[30%] rounded-none' />
                 </div>
-                <div className='mx-2 my-4'>
-                    {direct.length > 0 ? (<div className='grid gap-5'>{direct.map((dato, i) => (<p key={i} onClick={() => handleSelect(dato.name)} className='border h-16 w-[310px] flex items-center pl-5'>{`${dato.name}, ${dato.country}`}</p>))}</div>) : (<p>{''}</p>)}
+                <div className='mx-2 my-4'>{direct.length > 0 ?
+                    (<div className='grid gap-5'>
+                        {direct.map((dato, i) => (
+                            <Button key={i} onClick={() => handleSelect(dato.name)} className='px-5 py-2 rounded-none hover:border flex justify-between group'>
+                                <span>{`${dato.name}, ${dato.country}`}</span>
+                                <i className='pi pi-angle-right opacity-0 group-hover:opacity-100 transition-opacity'></i>
+                            </Button>
+                        ))}
+                    </div>) : (<Button>{''}</Button>)}
                 </div>
-            </Dialog>
+            </Sidebar>
         </>
     )
 }
